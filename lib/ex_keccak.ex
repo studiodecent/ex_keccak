f941cd311563aab45de743dc4c7c085baff41d1f
Serafeim Papastefanos <spapas@gmail.com>
2022-02-22 23:14:56 +0200

    Add tag 0.3.1

--- lib/ex_keccak.ex
+++ lib/ex_keccak.ex
@@ -3,7 +3,12 @@ defmodule ExKeccak do
   @moduledoc "README.md"
              |> File.read!()
 
-  use Rustler, otp_app: :ex_keccak, crate: :exkeccak, path: "native/exkeccak"
+  #use Rustler, otp_app: :ex_keccak, crate: :exkeccak, path: "native/exkeccak"
+  use RustlerPrecompiled,
+    otp_app: :ex_keccak,
+    crate: :exkeccak,
+    base_url: "https://github.com/me/my_project/releases/download/v0.3.1",
+    version: "0.3.1"
 
   def hash_256(_data), do: :erlang.nif_error(:nif_not_loaded)
 end
